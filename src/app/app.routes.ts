9f098e02ed348fc658243ebbbe0ddacc66107e49
Martin <martin.lecaros@3it.cl>
2025-10-14 19:18:32 -0300

    feat: navbar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    // { path: 'inicio', component: InicioComponent },
+    // { path: 'candidatos', component: CandidatosComponent },
+    // { path: 'nosotros', component: NosotrosComponent },
+    { path: '**', redirectTo: '/inicio' }
+];
