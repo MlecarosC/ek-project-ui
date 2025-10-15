e27d67c5dbabcdb43150d35988cc763b30980657
Martin <martin.lecaros@3it.cl>
2025-10-14 21:06:20 -0300

    feat: candidatos component added

---
+++ src/app/pages/candidatos/candidatos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidatos',
+  imports: [],
+  templateUrl: './candidatos.component.html',
+})
+export class CandidatosComponent { }
